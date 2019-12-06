b8dc81edb0cde2eac250a136b3593b0b4aed39cb
MrMusser <57725667MrMusser@users.noreply.github.com>
2019-12-06 11:06:03 -0500

    Added photo gallery with JavaScript functionality

---
+++ index.js
@@ -0,0 +1,63 @@
+const photo1a = document.getElementById('photo1a');
+const photo1b = document.getElementById('photo1b');
+const photo2 = document.getElementById('photo2');
+const photo3 = document.getElementById('photo3');
+const caption1 = document.getElementById('photo1-caption');
+const caption2 = document.getElementById('photo2-caption');
+const caption3 = document.getElementById('photo3-caption');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+let currentSlide = 1;
+
+next.onclick = () => {
+  if (currentSlide < 3) {
+    currentSlide++;
+  } else {
+    currentSlide = 1;
+  };
+  if (currentSlide === 1) {
+    photo1a.style.display = 'block';
+    photo1b.style.display = 'block';
+    photo3.style.display = 'none';
+    caption1.style.display = 'block';
+    caption3.style.display = 'none';
+  } else if (currentSlide === 2) {
+    photo2.style.display = 'block';
+    photo1a.style.display = 'none';
+    photo1b.style.display = 'none';
+    caption2.style.display = 'block';
+    caption1.style.display = 'none';
+  } else {
+    photo3.style.display = 'block';
+    photo2.style.display = 'none';
+    caption3.style.display = 'block';
+    caption2.style.display = 'none';
+  };
+};
+
+prev.onclick = () => {
+  if (currentSlide > 1) {
+    currentSlide--;
+  } else {
+    currentSlide = 3;
+  };
+  if (currentSlide === 1) {
+    photo1a.style.display = 'block';
+    photo1b.style.display = 'block';
+    photo2.style.display = 'none';
+    caption1.style.display = 'block';
+    caption2.style.display = 'none';
+  } else if (currentSlide === 2) {
+    photo2.style.display = 'block';
+    photo3.style.display = 'none';
+    caption2.style.display = 'block';
+    caption3.style.display = 'none';
+  } else {
+    photo3.style.display = 'block';
+    photo1a.style.display = 'none';
+    photo1b.style.display = 'none';
+    caption3.style.display = 'block';
+    caption1.style.display = 'none';
+  };
+};
