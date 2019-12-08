a0a178c06a43da8d0cd985dc0f333d4e83871a04
MrMusser <57725667MrMusser@users.noreply.github.com>
2019-12-08 08:18:08 -0500

    Added photo to gallery and adjusted display issues

--- index.js
+++ index.js
@@ -14,16 +14,18 @@ const photo1a = document.getElementById('photo1a');
 const photo1b = document.getElementById('photo1b');
 const photo2 = document.getElementById('photo2');
 const photo3 = document.getElementById('photo3');
+const photo4 = document.getElementById('photo4');
 const caption1 = document.getElementById('photo1-caption');
 const caption2 = document.getElementById('photo2-caption');
 const caption3 = document.getElementById('photo3-caption');
+const caption4 = document.getElementById('photo4-caption');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 
 let currentSlide = 1;
 
 next.onclick = () => {
-  if (currentSlide < 3) {
+  if (currentSlide < 4) {
     currentSlide++;
   } else {
     currentSlide = 1;
@@ -31,20 +33,25 @@ next.onclick = () => {
   if (currentSlide === 1) {
     photo1a.style.display = 'block';
     photo1b.style.display = 'block';
-    photo3.style.display = 'none';
+    photo4.style.display = 'none';
     caption1.style.display = 'block';
-    caption3.style.display = 'none';
+    caption4.style.display = 'none';
   } else if (currentSlide === 2) {
     photo2.style.display = 'block';
     photo1a.style.display = 'none';
     photo1b.style.display = 'none';
     caption2.style.display = 'block';
     caption1.style.display = 'none';
-  } else {
+  } else if (currentSlide === 3){
     photo3.style.display = 'block';
     photo2.style.display = 'none';
     caption3.style.display = 'block';
     caption2.style.display = 'none';
+  } else {
+    photo4.style.display = 'block';
+    photo3.style.display = 'none';
+    caption4.style.display = 'block';
+    caption3.style.display = 'none';
   };
 };
 
@@ -52,7 +59,7 @@ prev.onclick = () => {
   if (currentSlide > 1) {
     currentSlide--;
   } else {
-    currentSlide = 3;
+    currentSlide = 4;
   };
   if (currentSlide === 1) {
     photo1a.style.display = 'block';
@@ -65,11 +72,16 @@ prev.onclick = () => {
     photo3.style.display = 'none';
     caption2.style.display = 'block';
     caption3.style.display = 'none';
-  } else {
+  } else if (currentSlide === 3) {
     photo3.style.display = 'block';
+    photo4.style.display = 'none';
+    caption3.style.display = 'block';
+    caption4.style.display = 'none';
+  } else {
+    photo4.style.display = 'block';
     photo1a.style.display = 'none';
     photo1b.style.display = 'none';
-    caption3.style.display = 'block';
+    caption4.style.display = 'block';
     caption1.style.display = 'none';
   };
 };
